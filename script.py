3c7141689bbf46afbb8f43ab83e861768da652ae
Daniel <dangelidaniel20@gmail.com>
2021-12-09 17:57:26 +0100

    v0.4
    - Da aggiustare per divisione dati

--- script.py
+++ script.py
@@ -7,29 +7,20 @@ def csvinput():
             csvfile = input("Inserisci il path del file csv: ")
             with open(csvfile, newline="") as csvfile:
                 reader = csv.reader(csvfile, delimiter=";")
-                risk = [(row[1]) for row in reader if row[1] != "Risk"]
-                size = len(risk)
-                low = 0
-                mid = 0
-                high = 0
-                critical = 0
-                for value in risk:
-                    if value == "Medium":
-                        mid += 1
-                        continue
-                    elif value == "Low":
-                        low += 1
-                        continue
-                    elif value == "High":
-                        high += 1
-                    elif value == "Critical":
-                        critical += 1
-                        continue
-                low = int((low * 100) / size)
-                mid = int((mid * 100) / size)
-                high = int((high * 100) / size)
-                critical = int((critical * 100) / size)
-                results = [low, mid, high, critical]
+                next(reader)
+                dati = [(row[1], row[2]) for row in reader]
+                size = len(dati)
+                risklist = [0,0,0,0]
+                for value in dati:
+                    risk = value[0]
+                    host = value[1]
+                    :
+
+                #low = int((low * 100) / size)
+                #mid = int((mid * 100) / size)
+                #high = int((high * 100) / size)
+                #critical = int((critical * 100) / size)
+                #results = [low, mid, high, critical]
                 return results
         except ValueError:
             print("File non trovato, reinserire: ")
