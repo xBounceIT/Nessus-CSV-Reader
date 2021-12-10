d6cadc0a52ad56556105ee0eeee1a10bae17745d
Daniel D'Angeli <35002896+xBounceIT@users.noreply.github.com>
2021-12-10 22:12:05 +0100

    v1.1
    - Added report creation
    - Modified graph creation
    - Added multihost support

--- script.py
+++ script.py
@@ -1,5 +1,12 @@
 import csv
+import datetime
 import matplotlib.pyplot as plt
+
+date = datetime.datetime.now()
+date = date.strftime("%d-%m-%Y")
+
+def startup():
+    print("Nessus CSV Reader v1.1\n")
 
 def csvinput():
     while True:
@@ -33,25 +40,45 @@ def csvinput():
                     mid = dati.count(("Medium", host))                                              # Conta le medium
                     high = dati.count(("High", host))                                               # Conta le high
                     critical = dati.count(("Critical", host))                                       # Conta le critical
-                    low = int((low * 100) / lowtotal)                                               # Determina la percentuale in basse a tutte le low
-                    mid = int((mid * 100) / midtotal)                                               # Determina la percentuale in basse a tutte le medium
-                    high = int((high * 100) / hightotal)                                            # Determina la percentuale in basse a tutte le high
-                    critical = int((critical * 100) / criticaltotal)                                # Determina la percentuale in basse a tutte le critical
-                    print(host, "Low:", low, "Medium", mid, "High", high, "Critical", critical)              
-                results = [low, mid, high, critical]
-                return results
-        except ValueError:
+                    low = round(((low * 100) / lowtotal), 2)                                               # Determina la percentuale in basse a tutte le low
+                    mid = round(((mid * 100) / midtotal), 2)                                               # Determina la percentuale in basse a tutte le medium
+                    high = round(((high * 100) / hightotal), 2)                                            # Determina la percentuale in basse a tutte le high
+                    critical = round(((critical * 100) / criticaltotal), 2)                                # Determina la percentuale in basse a tutte le critical
+                    with open(f"Report_Severity_{date}.txt", "a") as result:
+                        result.write("\n-----------------------------------------------------------------\n")
+                        result.write("|                         ")
+                        result.write(str(host))
+                        result.write("                          |")
+                        result.write("\n-----------------------------------------------------------------\n")
+                        result.write("Low: ")
+                        result.write(str(low))
+                        result.write("%")
+                        result.write(" | Medium: ")
+                        result.write(str(mid))
+                        result.write("%")
+                        result.write(" | High: ")
+                        result.write(str(high))
+                        result.write("%")
+                        result.write(" | Critical: ")
+                        result.write(str(critical))
+                        result.write("%")
+                        result.write("\n-----------------------------------------------------------------\n")
+                result.close()
+                total = [lowtotal, midtotal, hightotal, criticaltotal]
+                return total
+        except FileNotFoundError:
             print("File non trovato, reinserire: ")
 
 def graph(values):
     labels = ["Low", "Medium", "High", "Critical"]
     plt.pie(values, labels=labels)
-    plt.title("Rapporto severity rilevate")
+    plt.title(date)
     plt.legend(bbox_to_anchor=(1.05,1.0), loc="upper left")
-    plt.savefig("torta.png")
+    plt.savefig(f"Grafico_Severity_{date}")
     return 0
 
 def main():
+    startup()
     values = csvinput()
     graph(values)
     return 0
@@ -60,4 +87,4 @@ if __name__ == "__main__":
   try:
     main()
   except KeyboardInterrupt:
-    exit(f"\nScript interrotto dall'utente")
+    exit("\nScript interrotto dall'utente")
